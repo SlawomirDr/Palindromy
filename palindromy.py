4a6d4068f1adf6c93fb018668731111908293865
Sławomir Drachal <drachal@tlen.pl>
2022-01-14 18:51:46 +0100

    Add palindromy.py

---
+++ palindromy.py
@@ -0,0 +1,11 @@
+def palindrom(wyraz):
+    lista_liter = list(palindrom)
+    for litera in palindrom:
+      return(bool(litera[0]==litera[-1])) 
+if (palindrom(2)):
+    print("palindrom")
+else:
+    print("to nie palindrom")
+
+palindrom("odo")
+
