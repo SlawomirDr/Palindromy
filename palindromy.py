dfc07ca5a39706ab18340e58267d74407f8bc91a
Sławomir Drachal <drachal@tlen.pl>
2022-01-15 17:20:09 +0100

    Add odd or even letters count palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -1,12 +1,20 @@
 def palindrom(wyraz):
-    lista_liter = list(wyraz)
     
-    wynik = (bool(lista_liter[0]==lista_liter[-1])) 
-
-    if wynik == True:
-        print("To jest palindrom")
+    lista_liter = list(wyraz)
+    ile_liter = len(lista_liter)
+    if (ile_liter) % 2 == 0:
+        print("To nie jest palindrom1")
+            
+        
     else:
-        print("To nie jest palindrom")
+        wynik = (bool(lista_liter[0]==lista_liter[-1])) 
+        if wynik == True:
+           print("To jest palindrom")
+        else:
+           print("To nie jest palindrom2")
 
-palindrom("saghj")
+    
+    
+
+palindrom("kajtr")
 
